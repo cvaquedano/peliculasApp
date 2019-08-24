878f1c5155fb79414cf2643616bb4befc1047020
Carlos Vaquedano <chvb2002@hotmail.com>
2019-08-24 10:00:09 -0600

    agregar iamgen y nombre de actores con ion chip

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MoviesService } from 'src/app/services/movies.service';
+import { PeliculaDetalle, Cast } from 'src/app/interfaces/interfaces';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-detalle',
@@ -9,20 +11,33 @@ import { MoviesService } from 'src/app/services/movies.service';
 export class DetalleComponent implements OnInit {
 
   @Input() id: string;
+  pelicula: PeliculaDetalle = {};
+  actores: Cast[] = [];
+  oculto = 150;
 
-  constructor(private moviesService: MoviesService) { }
+  slideOptActores = {
+    slidesPerView: 3.3,
+    freeMode: true,
+    spaceBetween: -5
+  };
+
+  constructor(private moviesService: MoviesService, private modaCtrl: ModalController) { }
 
   ngOnInit() {
 
     this.moviesService.getPeliculaDetalle(this.id)
     .subscribe( resp => {
-      console.log(resp);
+      this.pelicula = resp;
     });
 
     this.moviesService.getActoresPelicula(this.id)
     .subscribe( resp => {
-      console.log(resp);
+      this.actores = resp.cast;
     });
+  }
+
+  regresar() {
+    this.modaCtrl.dismiss();
   }
 
 }
